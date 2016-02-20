1b1b478fdda6e99bbdbc4f6c6346fb2bb1e227ea
Andrew Burnett <andrew@nospam.forme>
2016-02-20 22:20:30 +0000

    Add effect icons

--- ts/components/game/Minion.tsx
+++ ts/components/game/Minion.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 import EntityInPlay from "./EntityInPlay";
 import {EntityInPlayProps} from "../../interfaces";
-
+import {GameTag} from "../../enums";
 import InPlayCardArt from './InPlayCardArt';
 
 import Attack from './stats/Attack';
@@ -22,6 +22,7 @@ class Minion extends EntityInPlay<EntityInPlayProps, {}> {
 		var title = entity.getCardId();
 		var defaultAttack = null;
 		var defaultHealth = null;
+
 		if (this.props.cards && this.props.cards.has(entity.getCardId())) {
 			var data = this.props.cards.get(entity.getCardId());
 			title = data.name;
@@ -31,7 +32,12 @@ class Minion extends EntityInPlay<EntityInPlayProps, {}> {
 
 		return (
 			<div>
-				<InPlayCardArt taunt={entity.isTaunter()} legendary={entity.isLegendary()} cardId={entity.getCardId()} assetDirectory={this.props.assetDirectory}/>
+				<InPlayCardArt
+					taunt={entity.isTaunter()}
+					legendary={entity.isLegendary()}
+					entity={entity}
+					cardId={entity.getCardId()}
+					assetDirectory={this.props.assetDirectory}/>
 				<div className="stats">
 					<Attack attack={entity.getAtk()} default={defaultAttack}/>
 					<Health health={entity.getHealth()} damage={entity.getDamage()} default={defaultHealth}/>
