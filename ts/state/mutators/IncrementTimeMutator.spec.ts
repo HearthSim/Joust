d67df7b08e0d2747c300140ed17b1ba6b0e08ba0
Benedict Etzel <developer@beheh.de>
2016-03-06 16:23:42 +0100

    Improve IncrementTimeMutator spec

--- ts/state/mutators/IncrementTimeMutator.spec.ts
+++ ts/state/mutators/IncrementTimeMutator.spec.ts
@@ -3,42 +3,45 @@ import GameState from "../GameState";
 
 describe("IncrementTimeMutator", () => {
 
-	it("should have a default time of 1", () => {
+	it("should have a default time increment of 1", () => {
 		let mutator = new IncrementTimeMutator();
 		expect(mutator.time).toEqual(1);
 	});
 
-	it("should save the time to increment", () => {
-		let mutator = new IncrementTimeMutator(42);
-		expect(mutator.time).toEqual(42);
+	it("should save the time increment", () => {
+		let expected = 42;
+		let mutator = new IncrementTimeMutator(expected);
+		expect(mutator.time).toEqual(expected);
 	});
 
-	it("should have a mutable time", () => {
-		let mutator = new IncrementTimeMutator(42);
-		mutator.time = 1337;
-		expect(mutator.time).toEqual(1337);
-	});
-
-	it("should set the time of a new gamestate to 1", () => {
+	it("should have a mutable time increment", () => {
 		let mutator = new IncrementTimeMutator();
-		let state = new GameState();
-		expect(mutator.applyTo(state).getTime()).toEqual(1);
+		let expected = 42;
+		mutator.time = expected;
+		expect(mutator.time).toEqual(expected);
 	});
 
-	it("should set the time of a new gamestate to any value", () => {
+	it("should set the time of a new gamestate to the time increment", () => {
+		let state = new GameState();
 		let mutator = new IncrementTimeMutator(42);
-		let state = new GameState();
-		expect(mutator.applyTo(state).getTime()).toEqual(42);
+		let expected = mutator.time;
+		expect(mutator.applyTo(state).getTime()).toEqual(expected);
 	});
 
-	it("should increment the time of a gamestate by 1", () => {
-		let mutator1 = new IncrementTimeMutator();
+	it("should increase the time of a gamestate by the time increment", () => {
 		let state = new GameState();
+
+		let mutator1 = new IncrementTimeMutator();
+		let mutator2 = new IncrementTimeMutator(3);
+		let expected = mutator1.time;
+
 		state = state.apply(mutator1);
-		expect(state.getTime()).toEqual(1);
-		let mutator2 = new IncrementTimeMutator();
-		state = state.apply(mutator1);
-		expect(state.getTime()).toEqual(2);
+		expect(state.getTime()).toEqual(expected);
+
+		expected += mutator2.time;
+
+		state = state.apply(mutator2);
+		expect(state.getTime()).toEqual(expected);
 	});
 
 });
