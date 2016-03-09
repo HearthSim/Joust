d7557bc1b57e528d2e1ba2c06deed1d31d9294ac
Alexander Zeier <epikz37@gmail.com>
2016-03-09 17:38:54 +0100

    Display player rank next to name

--- ts/components/game/Player.tsx
+++ ts/components/game/Player.tsx
@@ -12,6 +12,7 @@ import HeroPower from './HeroPower';
 import Field from './Field';
 import Weapon from './Weapon';
 import Secrets from './Secrets';
+import Rank from './Rank';
 
 import {Zone, CardType} from '../../enums'
 import {OptionCallbackProps, CardDataProps, CardOracleProps, AssetDirectoryProps, TextureDirectoryProps} from "../../interfaces";
@@ -92,6 +93,12 @@ class Player extends React.Component<PlayerProps, {}> {
 		/>;
 		var name = this.props.player.getName() ? <div className="name">{this.props.player.getName()}</div> : null;
 
+		var rank = <Rank rank={this.props.player.getRank()}
+						 legendRank={this.props.player.getLegendRank()}
+						 assetDirectory={this.props.assetDirectory}
+						 textureDirectory={this.props.textureDirectory}
+		/>;
+
 		var crystals = [];
 		for (let i = 0; i < this.props.player.getResources(); i++) {
 			var crystalClassNames = ['crystal'];
@@ -119,7 +126,8 @@ class Player extends React.Component<PlayerProps, {}> {
 				<div className={classNames}>
 					{hand}
 					<div className="equipment">
-						<div>
+						<div className="player-info">
+							{rank}
 							{name}
 						</div>
 						<div></div>
@@ -144,7 +152,8 @@ class Player extends React.Component<PlayerProps, {}> {
 				<div className={classNames}>
 					{field}
 					<div className="equipment">
-						<div>
+						<div className="player-info">
+							{rank}
 							{name}
 						</div>
 						<div></div>
