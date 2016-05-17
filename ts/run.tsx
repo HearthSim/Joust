e51824b15d2c93b3d8664088e937d5232eb584c6
Benedict Etzel <developer@beheh.de>
2016-05-17 11:31:54 +0200

    Upgrade typings.json for typings 1.0

--- ts/run.tsx
+++ ts/run.tsx
@@ -1,4 +1,4 @@
-/// <reference path="../typings/browser.d.ts"/>
+/// <reference path="../typings/index.d.ts"/>
 /// <reference path="./global.d.ts"/>
 /// <reference path="../node_modules/immutable/dist/immutable.d.ts"/>
 
