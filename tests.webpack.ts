3fb499185cf7885db43d75ff3255f130087313a5
Benedict Etzel <developer@beheh.de>
2016-07-26 17:35:45 +0200

    Tests: Fix duplicate typings for Immutable.js

--- tests.webpack.ts
+++ tests.webpack.ts
@@ -1,6 +1,5 @@
 /// <reference path="./typings/index.d.ts"/>
 /// <reference path="./ts/global.d.ts"/>
-/// <reference path="./node_modules/immutable/dist/immutable.d.ts"/>
 
 var context = (require as any).context('./ts', true, /\.spec\.tsx?$/);
 context.keys().forEach(context);
