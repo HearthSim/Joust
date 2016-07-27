03a05d29bb579e970f582c9734529dc1bccb12bc
Benedict Etzel <developer@beheh.de>
2016-07-27 11:12:59 +0200

    Clean up run.tsx

--- ts/run.tsx
+++ ts/run.tsx
@@ -1,7 +1,7 @@
 /// <reference path="../typings/index.d.ts"/>
 /// <reference path="./global.d.ts"/>
 
-import {GameWidgetProps, CardData, JoustEventHandler} from "./interfaces";
+import {GameWidgetProps, CardData, JoustEventHandler, QueryCardMetadata} from "./interfaces";
 import Application from "./components/Joust";
 import GameWidget from "./components/GameWidget";
 import GameStateSink from "./state/GameStateSink";
@@ -11,7 +11,6 @@ import GameStateScrubber from "./state/GameStateScrubber";
 import * as http from "http";
 import * as https from "https";
 import * as URL from "url";
-import {QueryCardMetadata} from "./interfaces";
 import TexturePreloader from "./TexturePreloader";
 import {EventEmitter} from "events";
 import * as async from "async";
@@ -21,19 +20,19 @@ var ReactDOM = require('react-dom');
 
 class Launcher {
 
-	protected target: string | HTMLElement;
-	protected opts: GameWidgetProps;
-	protected queryCardMetadata: QueryCardMetadata;
-	protected startFromTurn: number;
-	protected turnCb: (turn: number) => void;
-	protected pause: boolean;
-	protected ref: GameWidget;
+	protected target:string | HTMLElement;
+	protected opts:GameWidgetProps;
+	protected queryCardMetadata:QueryCardMetadata;
+	protected startFromTurn:number;
+	protected turnCb:(turn:number) => void;
+	protected pause:boolean;
+	protected ref:GameWidget;
 
-	constructor(target: any) {
+	constructor(target:any) {
 		this.target = target;
 		this.opts = {
 			debug: false,
-			logger: (error: Error): void => {
+			logger: (error:Error):void => {
 				let message = error.message ? error.message : error;
 				console.error(message);
 			}
@@ -42,59 +41,59 @@ class Launcher {
 		this.opts.cardArtDirectory = "https://static.hsreplay.net/static/joust/card-art/";
 	}
 
-	public width(width: number): Launcher {
+	public width(width:number):Launcher {
 		this.opts.width = width;
 		return this;
 	}
 
-	public height(height: number): Launcher {
+	public height(height:number):Launcher {
 		this.opts.height = height;
 		return this;
 	}
 
-	public assets(assets: string): Launcher {
+	public assets(assets:string):Launcher {
 		this.opts.assetDirectory = assets;
 		return this;
 	}
 
-	public textures(a: any): Launcher {
+	public textures(a:any):Launcher {
 		console.warn('Launcher.textures() is no longer in use, use .cardArt() instead');
 		return this;
 	}
 
-	public cardArt(url: string): Launcher {
+	public cardArt(url:string):Launcher {
 		this.opts.cardArtDirectory = url;
 		return this;
 	}
 
-	public metadata(query: QueryCardMetadata): Launcher {
+	public metadata(query:QueryCardMetadata):Launcher {
 		this.queryCardMetadata = query;
 		return this;
 	}
 
-	public setOptions(opts: any): Launcher {
+	public setOptions(opts:any):Launcher {
 		for (var prop in opts) {
 			this.opts[prop] = opts[prop];
 		}
 		return this;
 	}
 
-	public onTurn(callback: (turn: number) => void) {
+	public onTurn(callback:(turn:number) => void) {
 		this.turnCb = callback;
 		return this;
 	}
 
-	public onToggleReveal(callback: (reveal: boolean) => void) {
+	public onToggleReveal(callback:(reveal:boolean) => void) {
 		this.opts.onToggleReveal = callback;
 		return this;
 	}
 
-	public onToggleSwap(callback: (swap: boolean) => void) {
+	public onToggleSwap(callback:(swap:boolean) => void) {
 		this.opts.onToggleSwap = callback;
 		return this;
 	}
 
-	public startPaused(paused?: boolean): Launcher {
+	public startPaused(paused?:boolean):Launcher {
 		this.pause = typeof paused === "undefined" ? true : !!paused;
 		return this;
 	}
@@ -102,83 +101,83 @@ class Launcher {
 	/**
 	 * @deprecated
 	 */
-	public turn(turn: number): Launcher {
+	public turn(turn:number):Launcher {
 		return this.startAtTurn(turn);
 	}
 
-	public startAtTurn(turn: number): Launcher {
+	public startAtTurn(turn:number):Launcher {
 		this.startFromTurn = turn;
 		return this;
 	}
 
-	public startRevealed(reveal: boolean): Launcher {
+	public startRevealed(reveal:boolean):Launcher {
 		this.opts.startRevealed = reveal;
 		return this;
 	}
 
-	public startSwapped(swap: boolean): Launcher {
+	public startSwapped(swap:boolean):Launcher {
 		this.opts.startSwapped = swap;
 		return this;
 	}
 
-	public logger(logger: (message: string | Error) => void): Launcher {
+	public logger(logger:(message:string | Error) => void):Launcher {
 		this.opts.logger = logger;
 		return this;
 	}
 
-	public events(cb: JoustEventHandler): Launcher {
+	public events(cb:JoustEventHandler):Launcher {
 		this.opts.events = cb;
 		this.track("init", {count: 1});
 		return this;
 	}
 
-	public debug(enable?: boolean): Launcher {
-		if(typeof enable === 'undefined' || enable === null) {
+	public debug(enable?:boolean):Launcher {
+		if (typeof enable === 'undefined' || enable === null) {
 			enable = true;
 		}
 		this.opts['debug'] = enable;
 		return this;
 	}
 
-	protected log(message:any): void {
+	protected log(message:any):void {
 		this.opts.logger(message);
 	}
 
-	protected track(event: string, values: Object, tags?: Object): void {
-		if(!this.opts.events) {
+	protected track(event:string, values:Object, tags?:Object):void {
+		if (!this.opts.events) {
 			return;
 		}
-		if(!tags) {
+		if (!tags) {
 			tags = {};
 		}
 		tags["joust_release"] = JOUST_RELEASE;
 		this.opts.events(event, values, tags);
 	}
 
-	public fromUrl(url: string): void {
+	public fromUrl(url:string):void {
 		var decoder = new HSReplayDecoder();
 		decoder.debug = this.opts.debug;
 		var tracker = new GameStateTracker();
 		var scrubber = new GameStateScrubber(null, this.startFromTurn);
-		if(this.turnCb) {
+		if (this.turnCb) {
 			scrubber.on("turn", this.turnCb);
 		}
 		var sink = new GameStateSink();
 		var preloader = new TexturePreloader(this.opts.cardArtDirectory, this.opts.assetDirectory);
-		if(preloader.canPreload()) {
+		if (preloader.canPreload()) {
 			preloader.consume();
 		}
 
-		if(url.match(/^\//) && location && location.protocol) {
+		if (url.match(/^\//) && location && location.protocol) {
 			let old = url;
 			url = location.protocol + url;
 		}
 		var opts = URL.parse(url) as any;
 		opts.withCredentials = false;
-		(opts.protocol == 'https:' ? https : http).get(opts, (message: http.IncomingMessage) => {
+		(opts.protocol == 'https:' ? https : http).get(opts, (message:http.IncomingMessage) => {
 			let success = (message.statusCode == 200);
 			this.track("replay_load_error", {error: success ? "f" : "t"}, {statusCode: message.statusCode});
-			if(!success) {
+			if (!success) {
 				this.log(new Error('Could not load replay (status code ' + message.statusCode + ')'));
 				return;
 			}
@@ -197,25 +196,25 @@ class Launcher {
 				}
 			], () => {
 				scrubber.play();
-				if(this.pause || (typeof this.pause === "undefined" && this.startFromTurn)) {
+				if (this.pause || (typeof this.pause === "undefined" && this.startFromTurn)) {
 					scrubber.pause();
 				}
 			});
 
 			message
-				.pipe(decoder) // json -> mutators
+				.pipe(decoder) // xml -> mutators
 				.pipe(tracker) // mutators -> latest gamestate
 				.pipe(scrubber) // gamestate -> gamestate emit on scrub past
 				.pipe(sink); // gamestate
 
-			if(this.opts.cardArtDirectory) {
+			if (this.opts.cardArtDirectory) {
 				decoder.pipe(preloader);
 			}
 		});
-		decoder.once('build', (build?: number) => {
+		decoder.once('build', (build?:number) => {
 			if (this.queryCardMetadata) {
 				let queryTime = Date.now();
-				this.queryCardMetadata(build || null, (cards: CardData[]) => {
+				this.queryCardMetadata(build || null, (cards:CardData[]) => {
 					this.ref.setCards(cards);
 					this.track('cards_received', {duration: (Date.now() - queryTime) / 1000}, {
 						cards: cards.length,
@@ -223,7 +222,7 @@ class Launcher {
 					});
 				});
 			}
-		}).on('error',this.log.bind(this))
+		}).on('error', this.log.bind(this))
 			.once('error', () => this.track("decoder_any_error", {count: 1}));
 
 		this.opts.sink = sink;
@@ -235,7 +234,7 @@ class Launcher {
 		this.track("starting_from_turn", {fromTurn: this.startFromTurn ? "t" : "f", turn: this.startFromTurn | null});
 	}
 
-	protected render(): void {
+	protected render():void {
 		this.opts.startupTime = +Date.now();
 		this.ref = ReactDOM.render(
 			React.createElement(GameWidget, this.opts),
@@ -245,29 +244,29 @@ class Launcher {
 }
 
 module.exports = {
-	renderApplication: (target: string) => {
+	renderApplication: (target:string) => {
 		ReactDOM.render(
 			React.createElement(Application),
 			document.getElementById(target)
 		);
 	},
 
-	renderHSReplay: (target: string, url: string, opts?: GameWidgetProps) => {
+	renderHSReplay: (target:string, url:string, opts?:GameWidgetProps) => {
 		new Launcher(target).setOptions(opts).fromUrl(url);
 	},
 
 	/**
 	 * @deprecated
 	 */
-	viewer: (target: string | HTMLElement) => {
+	viewer: (target:string | HTMLElement) => {
 		return new Launcher(target);
 	},
 
-	launcher: (target: string | HTMLElement) => {
+	launcher: (target:string | HTMLElement) => {
 		return new Launcher(target);
 	},
 
-	release: (): string => {
+	release: ():string => {
 		return JOUST_RELEASE;
 	}
 }
