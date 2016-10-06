HearthSim/Joust

commit 6ba31a28b1e74ca99f5aa4bd244278dd054048a5

Improve skip back behaviour when replay is paused

ts/state/GameStateScrubber.ts
@@ -295,7 +295,7 @@ export default class GameStateScrubber extends Stream.Duplex implements StreamSc
 		}
 		let turnStart = turnStartState.time;
 		let timeElapsed = this.currentTime - turnStart;
-		if (timeElapsed > 1.5 * this.speed * this.multiplier) {
+		if (!this.isPlaying() || timeElapsed > 1.5 * this.speed * this.multiplier) {
 			this.currentTime = turnStart;
 			this.update();
 		}

commit 0751109846ff33c026397a8e9bc1039ac8e7f217

Fix #157
Regression introduced in 6ba31a28b1e74ca99f5aa4bd244278dd054048a5.

ts/state/GameStateScrubber.ts
@@ -295,7 +295,7 @@ export default class GameStateScrubber extends Stream.Duplex implements StreamSc
 		}
 		let turnStart = turnStartState.time;
 		let timeElapsed = this.currentTime - turnStart;
-		if (!this.isPlaying() || timeElapsed > 1.5 * this.speed * this.multiplier) {
+		if (timeElapsed > (this.isPlaying() ? 1.5 * this.speed * this.multiplier : 0)) {
 			this.currentTime = turnStart;
 			this.update();
 		}