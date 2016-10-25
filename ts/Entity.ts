HearthSim/Joust

commit 8fe2b38804c040090685c27c4dbcc92cc395f38f

Fix minions being asleep when they shouldn't be
Fixes #131.

ts/Entity.ts
@@ -100,6 +100,16 @@ class Entity {
 		return this.getTag(GameTag.ZONE_POSITION);
 	}
 
+	public isAsleep(controller?: Entity): boolean {
+		return (
+			this.getTag(GameTag.NUM_TURNS_IN_PLAY) == 0 &&
+			this.getTag(GameTag.CHARGE) >= 0 &&
+			!this.getTag(GameTag.UNTOUCHABLE) &&
+			!this.getTag(GameTag.AUTOATTACK) &&
+			(!controller || controller.getTag(GameTag.CURRENT_PLAYER) == 1)
+		);
+	}
+
 	public getTag(key:number):number {
 		return this.tags ? (+this.tags.get('' + key) || 0) : 0;
 	}

ts/components/game/Minion.tsx
@@ -46,6 +46,10 @@ class Minion extends EntityInPlay<EntityInPlayProps> {
 			})
 		}
 
+		if (data.mechanics && data.mechanics.indexOf("AUTOATTACK") !== -1) {
+			entity = entity.setTag(GameTag.AUTOATTACK, 1);
+		}
+
 		let components = [
 			<InPlayCardArt
 				key="art"

ts/components/game/visuals/InPlayCardArt.tsx
@@ -96,9 +96,7 @@ class InPlayCardArt extends React.Component<EntityProps, {}> {
 			});
 		}
 
-		if (entity.getTag(GameTag.NUM_TURNS_IN_PLAY) == 0 &&
-			entity.getTag(GameTag.CHARGE) <= 0 &&
-			(!controller || controller.getTag(GameTag.CURRENT_PLAYER))) {
+		if (entity.isAsleep(controller)) {
 			images.push({
 				image: "effect_sleep.png",
 				classes: ["effect-sleep"]

commit 4d1877eb0e736bb98e970b9c42bb82188e5e164d

Fix minions with charge being shown as asleep
Regression introduced in 8fe2b38804c040090685c27c4dbcc92cc395f38f.

ts/Entity.ts
@@ -123,7 +123,7 @@ export default class Entity {
 	public isAsleep(controller?: Entity): boolean {
 		return (
 			this.getTag(GameTag.NUM_TURNS_IN_PLAY) == 0 &&
-			this.getTag(GameTag.CHARGE) >= 0 &&
+			this.getTag(GameTag.CHARGE) == 0 &&
 			!this.getTag(GameTag.UNTOUCHABLE) &&
 			!this.getTag(GameTag.AUTOATTACK) &&
 			(!controller || controller.getTag(GameTag.CURRENT_PLAYER) == 1)